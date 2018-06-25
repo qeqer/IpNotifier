1255d8df6f8413eaf8e2b3cce2aa6a0d3aec60a0
qeqer <macworlex@gmail.com>
2018-06-25 13:24:28 +0300

    v1.0
    Ready site + python script to detect ip and send to server

--- api.php
+++ api.php
@@ -5,12 +5,42 @@
 		throw new ErrorException ($err_msg, 0, $err_severity, $err_file, $err_line);
 	});
 	$allOk = TRUE;
+	$host = "localhost";
+	$user = "qeqer";
+	$password = "Z0ltIIeFrexYuEuf";
+	$db = "qeqer";
 	try {
+		$con = new mysqli($host, $user, $password, $db);
+		$con->set_charset("utf8");
 		$vars = get_angular_request_payload(); //Получаем данные
-		error_log($vars);
-
+		$resultT = [];
+		switch ($vars["command"]) {
+			case "saveIP":
+				date_default_timezone_set('Europe/Moscow');
+				$insQuery = "INSERT INTO ip VALUES (\"".$vars['ip']."\", \"".date("Y-m-d H:i:s", time())."\")";
+				$res = $con->query($insQuery);
+				if (!$res) {
+					error_log($insQuery);
+					error_log("Fail: ".$con->error);
+					$allOk = FALSE;
+				}
+				break;
+			case "loadInfo":
+				$selQuery = "SELECT * FROM ip ORDER BY Time DESC";
+				$res = $con->query($selQuery);
+				if (!$res) {
+					error_log($selQuery);
+					error_log("Fail: ".$con->error);
+					break;
+				}
+				$temp = $res -> fetch_array(MYSQLI_BOTH);
+				$resultT[] = $temp['Address'];
+				$resultT[] = $temp['Time'];
+				echo json_encode($resultT, JSON_UNESCAPED_UNICODE, true);
+		}
 	} catch (Exception $e) {
 		error_log("!!!".$e->getMessage()."\n");
+		header("HTTP/1.1 444 Fiasko");
 		exit(0);
 	}
 	if ($allOk) {
--- test.py
+++ test.py
@@ -1,10 +1,20 @@
 import requests
+import subprocess
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("google.com", 80))
+ip = s.getsockname()[0]
+
+s.close()
+# subprocess.call(["tracert", "google.com"])
+
 
 url = "http://localhost/ipnotifier/api.php"
-data = '{"ip": "0.0.0.0"}'
+data = {"command": "saveIP","ip": ip}
+
 res = ""
 try:
-	res = requests.post(url, json = data)
+	res = requests.post(url, json=data)
 except Exception as inst:
 	print(type(inst))
 	print(inst.args)
